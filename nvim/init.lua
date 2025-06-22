d14a76f457fee634d027a595091c3667f1dd410c
NWalker4483 <nilezwalker@gmail.com>
2025-06-22 14:52:17 -0400

    Added Test Data

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,110 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+-- ========================================================================== --
+-- ==                           EDITOR SETTINGS                            == --
+-- ========================================================================== --
+require('config.lazy')
+-- Auto-change to directory when opening a directory argument
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    local args = vim.fn.argv()
+    if #args == 1 and vim.fn.isdirectory(args[1]) == 1 then
+      -- Change Neovim's working directory to the opened directory
+      vim.cmd("cd " .. vim.fn.fnamemodify(args[1], ":p"))
+    end
+  end,
+})
+-- vim.opt.number = true
+-- vim.opt.mouse = "a"
+-- vim.opt.ignorecase = true
+-- vim.opt.smartcase = true
+-- vim.opt.hlsearch = false
+-- vim.opt.wrap = true
+-- vim.opt.breakindent = true
+-- vim.opt.tabstop = 2
+-- vim.opt.shiftwidth = 2
+-- vim.opt.expandtab = false
+
+-- vim.g.netrw_banner = 0
+-- vim.g.netrw_winsize = 30
+-- -- -- 
+-- -- ========================================================================== --
+-- -- ==                             KEYBINDINGS                              == --
+-- -- ========================================================================== --
+
+-- -- Space as leader key
+-- vim.g.mapleader = " "
+
+-- -- Shortcuts
+-- vim.keymap.set({ "n", "x", "o" }, "<leader>h", "^")
+-- vim.keymap.set({ "n", "x", "o" }, "<leader>l", "g_")
+-- vim.keymap.set("n", "<leader>a", ":keepjumps normal! ggVG<cr>")
+
+-- -- Basic clipboard interaction
+-- vim.keymap.set({ "n", "x" }, "gy", '"+y') -- copy
+-- vim.keymap.set({ "n", "x" }, "gp", '"+p') -- paste
+
+-- -- Delete text
+-- vim.keymap.set({ "n", "x" }, "x", '"_x')
+-- vim.keymap.set({ "n", "x" }, "X", '"_d')
+
+-- -- Commands
+-- vim.keymap.set("n", "<leader>w", "<cmd>write<cr>")
+-- vim.keymap.set("n", "<leader>bq", "<cmd>bdelete<cr>")
+-- vim.keymap.set("n", "<leader>bl", "<cmd>buffer #<cr>")
+-- vim.keymap.set("n", "<leader><space>", "<cmd>buffers<cr>:buffer<Space>")
+-- vim.keymap.set("n", "<leader>e", "<cmd>Lexplore %:p:h<cr>")
+-- vim.keymap.set("n", "<leader>E", "<cmd>Lexplore<cr>")
+
+-- local function netrw_mapping()
+-- 	local bufmap = function(lhs, rhs)
+-- 		local opts = { buffer = true, remap = true }
+-- 		vim.keymap.set("n", lhs, rhs, opts)
+-- 	end
+
+-- 	-- close window
+-- 	bufmap("<leader>e", ":Lexplore<cr>")
+-- 	bufmap("<leader>E", ":Lexplore<cr>")
+
+-- 	-- Go back in history
+-- 	bufmap("H", "u")
+
+-- 	-- Go up a directory
+-- 	bufmap("h", "-^")
+
+-- 	-- Open file/directory
+-- 	bufmap("l", "<cr>")
+
+-- 	-- Open file/directory then close explorer
+-- 	bufmap("L", "<cr>:Lexplore<CR>")
+
+-- 	-- Toggle dotfiles
+-- 	bufmap(".", "gh")
+-- end
+
+-- -- ========================================================================== --
+-- -- ==                               COMMANDS                               == --
+-- -- ========================================================================== --
+
+-- vim.api.nvim_create_user_command("ReloadConfig", "source $MYVIMRC", {})
+
+-- local group = vim.api.nvim_create_augroup("user_cmds", { clear = true })
+
+-- vim.api.nvim_create_autocmd("TextYankPost", {
+-- 	group = group,
+-- 	desc = "Highlight on yank",
+-- 	callback = function()
+-- 		vim.highlight.on_yank({ higroup = "Visual", timeout = 200 })
+-- 	end,
+-- })
+
+-- vim.api.nvim_create_autocmd("FileType", {
+-- 	pattern = { "help", "man" },
+-- 	group = group,
+-- 	command = "nnoremap <buffer> q <cmd>quit<cr>",
+-- })
+
+-- vim.api.nvim_create_autocmd("FileType", {
+-- 	pattern = "netrw",
+-- 	group = group,
+-- 	desc = "Keybindings for netrw",
+-- 	callback = netrw_mapping,
+-- })
---
+++ nvim/lua/plugins/snacks.lua
@@ -0,0 +1,208 @@
+return {
+  -- Snacks.nvim - Collection of QoL plugins
+  {
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    ---@type snacks.Config
+    opts = {
+      -- Core snacks features
+      bigfile = { enabled = true },
+      dashboard = { 
+        enabled = true,
+        sections = {
+          { section = "header" },
+          { section = "keys", gap = 1, padding = 1 },
+          { section = "startup" },
+        },
+      },
+      indent = { enabled = true },
+      input = { enabled = true },
+      notifier = { enabled = true },
+      quickfile = { enabled = true },
+      image = { enabled = true },
+      scroll = { enabled = true },
+      statuscolumn = { enabled = true },
+      words = { enabled = true },
+      zen = { enabled = true },
+      bufdelete = { enabled = true },
+      rename = { enabled = true },
+      lazygit = { enabled = true },
+      git = { enabled = true },
+      gitbrowse = { enabled = true },
+      explorer = { enabled = true },
+      picker = { enabled = true },
+      toggle = { enabled = true },
+      scratch = { enabled = true },
+      animate = { enabled = true },
+      
+      -- Terminal
+      terminal = { 
+        enabled = true,
+        -- Configure default terminal layout
+        win = {
+          style = "terminal",
+          position = "bottom", -- Default to bottom position
+          height = 0.3,        -- 30% of screen height
+          width = 0.9,         -- 90% of screen width when floating
+          border = "rounded",
+        },
+      },
+    },
+    keys = {
+      -- File Management
+      { "<leader>e", function() Snacks.explorer() end, desc = "Explorer" },
+      { "<leader>E", function() Snacks.explorer({ cwd = vim.fn.expand("%:p:h") }) end, desc = "Explorer (cwd)" },
+      
+      -- Git Integration
+      { "<leader>gg", function() Snacks.lazygit() end, desc = "Lazygit" },
+      { "<leader>gb", function() Snacks.git.blame_line() end, desc = "Git Blame Line" },
+      { "<leader>gB", function() Snacks.gitbrowse() end, desc = "Git Browse" },
+      { "<leader>gf", function() Snacks.lazygit.log_file() end, desc = "Lazygit Current File History" },
+      { "<leader>gl", function() Snacks.lazygit.log() end, desc = "Lazygit Log (cwd)" },
+      
+      -- File Operations
+      { "<leader>r", function() Snacks.rename.rename_file() end, desc = "Rename File" },
+      
+      -- Finder/Picker
+      { "<leader>ff", function() Snacks.picker.files() end, desc = "Find Files" },
+      { "<leader>fg", function() Snacks.picker.grep() end, desc = "Live Grep" },
+      { "<leader>fb", function() Snacks.picker.buffers() end, desc = "Find Buffers" },
+      { "<leader>fh", function() Snacks.picker.help() end, desc = "Find Help" },
+      { "<leader>fr", function() Snacks.picker.recent() end, desc = "Recent Files" },
+      { "<leader>fc", function() Snacks.picker.commands() end, desc = "Commands" },
+      { "<leader>fk", function() Snacks.picker.keymaps() end, desc = "Key Maps" },
+      
+      -- Toggle Features
+      { "<leader>uw", function() Snacks.toggle.option("wrap", { name = "Wrap" }) end, desc = "Toggle Wrap" },
+      { "<leader>ul", function() Snacks.toggle.option("relativenumber", { name = "Relative Number" }) end, desc = "Toggle Relative Number" },
+      { "<leader>un", function() Snacks.toggle.option("number", { name = "Line Numbers" }) end, desc = "Toggle Line Numbers" },
+      { "<leader>us", function() Snacks.toggle.option("spell", { name = "Spelling" }) end, desc = "Toggle Spelling" },
+      { "<leader>uh", function() Snacks.toggle.inlay_hints() end, desc = "Toggle Inlay Hints" },
+      
+      -- Scratch/Notes
+      { "<leader>.", function() Snacks.scratch() end, desc = "Toggle Scratch Buffer" },
+      { "<leader>S", function() Snacks.scratch.select() end, desc = "Select Scratch Buffer" },
+      
+      -- Words (references/navigation)
+      { "]]", function() Snacks.words.jump(vim.v.count1) end, desc = "Next Reference", mode = { "n", "t" } },
+      { "[[", function() Snacks.words.jump(-vim.v.count1) end, desc = "Prev Reference", mode = { "n", "t" } },
+      
+      -- Utilities
+      { "<leader>z", function() Snacks.zen() end, desc = "Toggle Zen Mode" },
+      { "<leader>n", function() Snacks.notifier.show_history() end, desc = "Notification History" },
+      { "<leader>bd", function() Snacks.bufdelete() end, desc = "Delete Buffer" },
+      
+      -- Terminal shortcuts
+      { "<c-/>", function() Snacks.terminal() end, desc = "Toggle Terminal" },
+      { "<c-_>", function() Snacks.terminal() end, desc = "Toggle Terminal (which_key_ignore)" },
+      { "<leader>tt", function() Snacks.terminal() end, desc = "Toggle Terminal" },
+      { "<leader>tf", function() Snacks.terminal(nil, { win = { position = "float" } }) end, desc = "Terminal Float" },
+      { "<leader>th", function() Snacks.terminal(nil, { win = { position = "bottom", height = 0.3 } }) end, desc = "Terminal Horizontal" },
+      { "<leader>tv", function() Snacks.terminal(nil, { win = { position = "right", width = 0.4 } }) end, desc = "Terminal Vertical" },
+      { "<leader>ts", function() 
+        -- Split current terminal
+        Snacks.terminal(nil, { win = { position = "bottom", height = 0.3 } })
+      end, desc = "Terminal Split" },
+      
+      -- Split screen commands
+      { "<leader>sv", "<cmd>vsplit<cr>", desc = "Vertical Split" },
+      { "<leader>sh", "<cmd>split<cr>", desc = "Horizontal Split" },
+      { "<leader>sc", "<cmd>close<cr>", desc = "Close Split" },
+      { "<leader>so", "<cmd>only<cr>", desc = "Close Other Splits" },
+      
+      -- Window navigation
+      { "<C-h>", "<C-w>h", desc = "Go to Left Window" },
+      { "<C-j>", "<C-w>j", desc = "Go to Lower Window" },
+      { "<C-k>", "<C-w>k", desc = "Go to Upper Window" },
+      { "<C-l>", "<C-w>l", desc = "Go to Right Window" },
+      
+      -- Window resizing
+      { "<C-Up>", "<cmd>resize +2<cr>", desc = "Increase Window Height" },
+      { "<C-Down>", "<cmd>resize -2<cr>", desc = "Decrease Window Height" },
+      { "<C-Left>", "<cmd>vertical resize -2<cr>", desc = "Decrease Window Width" },
+      { "<C-Right>", "<cmd>vertical resize +2<cr>", desc = "Increase Window Width" },
+      
+      -- Tab management
+      { "<leader>tn", "<cmd>tabnew<cr>", desc = "New Tab" },
+      { "<leader>tc", "<cmd>tabclose<cr>", desc = "Close Tab" },
+      { "<leader>to", "<cmd>tabonly<cr>", desc = "Close Other Tabs" },
+      { "[t", "<cmd>tabprevious<cr>", desc = "Previous Tab" },
+      { "]t", "<cmd>tabnext<cr>", desc = "Next Tab" },
+      
+      -- Buffer navigation
+      { "[b", "<cmd>bprevious<cr>", desc = "Previous Buffer" },
+      { "]b", "<cmd>bnext<cr>", desc = "Next Buffer" },
+      
+      -- Terminal mode keybindings
+      { "<C-/>", function() Snacks.terminal() end, desc = "Toggle Terminal", mode = "t" },
+      { "<esc><esc>", "<c-\\><c-n>", desc = "Enter Normal Mode", mode = "t" },
+      { "<C-h>", "<cmd>wincmd h<cr>", desc = "Go to Left Window", mode = "t" },
+      { "<C-j>", "<cmd>wincmd j<cr>", desc = "Go to Lower Window", mode = "t" },
+      { "<C-k>", "<cmd>wincmd k<cr>", desc = "Go to Upper Window", mode = "t" },
+      { "<C-l>", "<cmd>wincmd l<cr>", desc = "Go to Right Window", mode = "t" },
+      
+      -- File type specific shortcuts
+      { "<leader>f", group = "Find" },
+      { "<leader>g", group = "Git" },
+      { "<leader>u", group = "UI Toggle" },
+      { "<leader>t", group = "Tabs/Terminal" },
+      { "<leader>m", group = "Markview" },
+      { "<leader>c", group = "CSV" },
+      
+      -- CSV commands (available globally, work when in CSV files)
+      { "<leader>cv", "<cmd>CsvViewToggle<cr>", desc = "Toggle CSV View" },
+      { "<leader>ce", "<cmd>CsvViewEnable<cr>", desc = "Enable CSV View" },
+      { "<leader>cd", "<cmd>CsvViewDisable<cr>", desc = "Disable CSV View" },
+    },
+  },
+
+  -- Treesitter for syntax highlighting (separate plugin)
+  {
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    dependencies = { "OXY2DEV/markview.nvim" }, -- Ensure markview loads first
+    config = function()
+      require('nvim-treesitter.configs').setup({
+        ensure_installed = {
+          -- Core
+          'lua', 'vim', 'vimdoc', 'query',
+          -- Web
+          'html', 'css', 'javascript', 'typescript', 'tsx',
+          'json', 'yaml', 'toml',
+          -- Documents & markup
+          'markdown', 'markdown_inline', 'typst',
+          -- Programming languages  
+          'regex', 'bash', 'python', 'rust', 'go', 'c', 'cpp',
+          -- Frontend frameworks
+          'svelte', 'vue', 'scss',
+          -- CSV and other data formats
+          'csv',
+        },
+        auto_install = true,
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = false,
+        },
+        indent = {
+          enable = true,
+        },
+      })
+    end,
+  },
+
+  -- Optional: Icons support (separate plugin)
+  {
+    "nvim-tree/nvim-web-devicons",
+    lazy = true,
+  },
+  
+  -- Optional: which-key for better toggle descriptions (separate plugin)
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("which-key").setup()
+    end,
+  },
+}
